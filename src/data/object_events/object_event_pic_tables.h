b4d4300ab5169c6e11bb6a84889304d461f04c2b
cilerba <maxdcordeiro@gmail.com>
2022-06-05 20:20:44 -0400

    Fix incorrectly defined ObjectEventPicTable for Worker overworld

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -5514,13 +5514,13 @@ static const struct SpriteFrameImage sPicTable_RubySapphireMay[] = {
 };
 
 const struct SpriteFrameImage gObjectEventPicTable_Worker[] = {
-    overworld_frame(gObjectEventPic_Worker, 4, 4, 0),
-    overworld_frame(gObjectEventPic_Worker, 4, 4, 1),
-    overworld_frame(gObjectEventPic_Worker, 4, 4, 2),
-    overworld_frame(gObjectEventPic_Worker, 4, 4, 3),
-    overworld_frame(gObjectEventPic_Worker, 4, 4, 4),
-    overworld_frame(gObjectEventPic_Worker, 4, 4, 5),
-    overworld_frame(gObjectEventPic_Worker, 4, 4, 6),
-    overworld_frame(gObjectEventPic_Worker, 4, 4, 7),
-    overworld_frame(gObjectEventPic_Worker, 4, 4, 8),
+    overworld_frame(gObjectEventPic_Worker, 2, 4, 0),
+    overworld_frame(gObjectEventPic_Worker, 2, 4, 1),
+    overworld_frame(gObjectEventPic_Worker, 2, 4, 2),
+    overworld_frame(gObjectEventPic_Worker, 2, 4, 3),
+    overworld_frame(gObjectEventPic_Worker, 2, 4, 4),
+    overworld_frame(gObjectEventPic_Worker, 2, 4, 5),
+    overworld_frame(gObjectEventPic_Worker, 2, 4, 6),
+    overworld_frame(gObjectEventPic_Worker, 2, 4, 7),
+    overworld_frame(gObjectEventPic_Worker, 2, 4, 8),
 };
